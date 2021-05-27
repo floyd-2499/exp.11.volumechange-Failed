bbdb4872b4f15b62624bbea15d3016e999e266f7
floyd-2499 <79973317+floyd-2499@users.noreply.github.com>
2021-05-27 22:09:56 +0530

    Add files via upload

---
+++ 10. work on bluetooth btn click/index.js
@@ -0,0 +1,29 @@
+console.log("Hey Floyd");
+
+// // 1. exp
+// const blueBtn = document.querySelector(".btn");
+// blueBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   console.log("Hey boss. Button is working");
+// });
+// // yeaa... working
+
+// // 2.exp
+// const blueButton = document.querySelector(".btn");
+// blueButton.addEventListener('volumeupbutton', (e) => {
+//   e.preventDefault();
+//   console.log("Yeaaa... Blutooth btn is working");
+// });
+
+
+// document.addEventListener('volumeupbutton', () => {
+//  console.log("Yeaaa... Blutooth btn is working");
+//   }, false);
+
+// function myFunction() {
+//     console.log('Boss ');
+// }
+
+function myFunction() {
+     console.log("Yeaaa... Blutooth btn is working");
+  }
